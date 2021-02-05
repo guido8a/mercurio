205f32185c4c5534a13715b0fec62ed750720e06
fabricio4077 <orejona2501>
2021-02-05 16:54:09 -0500

    dominios

---
+++ grails-app/domain/seguridad/Accs.groovy
@@ -0,0 +1,30 @@
+package seguridad
+
+class Accs {
+
+    Accn accion
+    String url
+    Date fecha
+    String desde
+
+    static mapping = {
+        table 'accs'
+        cache usage: 'read-write', include: 'non-lazy'
+        id column: 'accs__id'
+        id generator: 'identity'
+        version false
+        columns {
+            id column: 'accs__id'
+            accion column: 'accn__id'
+            url column: 'accs_url'
+            fecha column: 'accsfcha'
+            desde column: 'accsdsde'
+        }
+    }
+    static constraints = {
+        url(size: 1..255, blank: true, nullable: true)
+        desde(size: 1..255, blank: true, nullable: true)
+        fecha(blank: true, nullable: true)
+        accion(blank: false, nullable: false)
+    }
+}
